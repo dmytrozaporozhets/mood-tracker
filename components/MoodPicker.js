ce35f83c4e2b8f5f54ce695bfd7112b6032787d8
Dmytro Zaporozhets <dmytrozaporozhets.kh@gmail.com>
2025-05-10 15:27:01 +0300

    Create MoodCard component for displaying mood entries. Refactor MoodPicker to sort mood list by date.

--- components/MoodPicker.js
+++ components/MoodPicker.js
@@ -1,20 +1,43 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect,useRef } from 'react';
 import { View, Text, TouchableOpacity, FlatList } from 'react-native';
 import MoodButton from './MoodButton';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import MoodPickerStyle from '../styles/components/MoodPicker';
 import { moods } from '../constants/moods';
+import MoodCard from '../components/MoodCard'
 
 const STORAGE_KEY = 'mood-list';
 
 const MoodPicker = () => {
     const [selectedMood, setSelectedMood] = useState(null);
     const [moodList, setMoodList] = useState([]);
+    const flatListRef = useRef(null);
+
+    const handleSave = async () => {
+        if (!selectedMood) return;
+        const newItem = { ...selectedMood, date: new Date().toISOString(), id: Date.now(), };
+        const updatedList = [...moodList, newItem];
+        const sortedList = [...updatedList].sort((a, b) => new Date(b.date) - new Date(a.date));
+
+
+        try {
+            await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(sortedList));
+            setMoodList(sortedList);
+            setSelectedMood(null);
+            flatListRef.current?.scrollToOffset({ offset: 0, animated: true });
+        } catch (e) {
+            console.error('Failed to save mood', e);
+        }
+    };
 
     const loadMoods = async () => {
         try {
             const stored = await AsyncStorage.getItem(STORAGE_KEY);
-            if (stored) setMoodList(JSON.parse(stored));
+            if (stored) {
+                const parsed = JSON.parse(stored);
+                const sortedList = [...parsed].sort((a, b) => new Date(b.date) - new Date(a.date));
+                setMoodList(sortedList);
+            }
         } catch (e) {
             console.error('Failed to load moods', e);
         }
@@ -23,19 +46,6 @@ const MoodPicker = () => {
     useEffect(() => {
         loadMoods();
     }, []);
-
-    const handleSave = async () => {
-        if (!selectedMood) return;
-        const newList = [...moodList, { ...selectedMood, date: new Date().toISOString() }];
-
-        try {
-            await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(newList));
-            setMoodList(newList);
-            setSelectedMood(null);
-        } catch (e) {
-            console.error('Failed to save mood', e);
-        }
-    };
 
     return (
         <View style={MoodPickerStyle.container}>
@@ -64,11 +74,12 @@ const MoodPicker = () => {
                 data={moodList}
                 keyExtractor={(item, index) => index.toString()}
                 renderItem={({ item }) => (
-                    <Text style={MoodPickerStyle.historyItem}>
-                        {item.emoji} - {item.label} - {new Date(item.date).toLocaleString()}
-                    </Text>
+                    <MoodCard item={item}/>
                 )}
                 style={{ marginTop: 20 }}
+                contentContainerStyle={{ paddingBottom: 100 }}
+                ref={flatListRef}
+                showsVerticalScrollIndicator={false}
             />
         </View>
     );
