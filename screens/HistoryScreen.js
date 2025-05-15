cee36a2b498dc56494a2d199b6ef7257d6574814
Dmytro Zaporozhets <dmytrozaporozhets.kh@gmail.com>
2025-05-15 17:20:56 +0300

    refactor: add styles on screens.

--- screens/HistoryScreen.js
+++ screens/HistoryScreen.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import {sg} from '../styling'
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const HistoryScreen = () => {
+  const insets = useSafeAreaInsets();
   return (
-    <View>
+    <View style={[sg.flex,sg.aICenter,sg.jCCenter,{paddingTop:insets.top}]} >
       <Text>History Screen</Text>
     </View>
   );
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import {sg} from '../styling'
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const HomeScreen = () => {
+  const insets = useSafeAreaInsets();
   return (
-    <View style={{ flex: 1 }}>
+    <View style={[sg.flex,sg.aICenter,sg.jCCenter,{paddingTop:insets.top}]} >
       <Text>Home Screen</Text>
     </View>
   );
