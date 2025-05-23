0672cccf4a025839fab22354f430389b6f6f7d0f
Dmytro <dmytrozaporozhets.kh@gmail.com>
2025-05-23 14:07:30 +0300

    fix: Fix app launch issue after migrating to TypeScript

--- src/hooks/useLanguage.tsx
+++ src/hooks/useLanguage.tsx
@@ -1,6 +1,6 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as Localization from 'expo-localization';
-import i18n from 'i18next';
+import i18n, { i18n as I18nType } from 'i18next';
 import { useEffect, useState } from 'react';
 import { initReactI18next } from 'react-i18next';
 
@@ -8,26 +8,30 @@ import { LANGUAGE_STORAGE_KEY } from '../constants/storage';
 import en from '../utils/i18n/locales/en';
 import ua from '../utils/i18n/locales/ua';
 
+type LanguageCode = 'en' | 'ua';
+
 const resources = {
   en,
   ua,
-};
+} as const;
 
-const getInitialLanguage = async () => {
+const getInitialLanguage = async (): Promise<LanguageCode> => {
   try {
     const storedLang = await AsyncStorage.getItem(LANGUAGE_STORAGE_KEY);
     if (storedLang && Object.keys(resources).includes(storedLang)) {
-      return storedLang;
+      return storedLang as LanguageCode;
     }
   } catch (e) {
     console.warn('Language load failed from storage', e);
   }
 
   const deviceLang = Localization.getLocales()[0]?.languageCode;
-  return Object.keys(resources).includes(deviceLang) ? deviceLang : 'en';
+  return Object.keys(resources).includes(deviceLang as LanguageCode)
+    ? (deviceLang as LanguageCode)
+    : 'en';
 };
 
-export const useLanguage = () => {
+export const useLanguage = (): { isLanguageReady: boolean; i18n: I18nType } => {
   const [isLanguageReady, setIsLanguageReady] = useState(false);
 
   useEffect(() => {
